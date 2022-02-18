01c2a9d3674a22c1da938362c3bed932cbcd11d9
Sophia Lyngsø <Sophia@Liselottes-MacBook-Pro-2.local>
2022-02-18 14:13:06 +0100

    opdateret PCA med plots, new box plots started

--- Scripts/PCA.py
+++ Scripts/PCA.py
@@ -10,6 +10,7 @@ from load_data import *
 import matplotlib.pyplot as plt
 from scipy.linalg import svd
 
+
 cols = range(1, 10) 
 X_PCA = X[:, cols]
 attributeNames_PCA = np.array(attributeNames[1:]);
@@ -44,13 +45,16 @@ print('Ran PCA')
 
 # exercise 2.1.4
 # (requires data structures from ex. 2.2.1 and 2.2.3)
-from ex2_1_1 import *
+#from ex2_1_1 import *
 
 from matplotlib.pyplot import figure, plot, title, xlabel, ylabel, show, legend
 from scipy.linalg import svd
 
 # Subtract mean value from data
 Y = X - np.ones((N,1))*X.mean(0)
+#if we want to standardize by dividing by the standard deviation
+Y = Y*(1/np.std(Y,0))
+
 
 # PCA by computing SVD of Y
 U,S,Vh = svd(Y,full_matrices=False)
@@ -80,4 +84,44 @@ ylabel('PC{0}'.format(j+1))
 # Output result to screen
 show()
 
-print('Ran Exercise 2.1.4')
+
+
+pcs = [0,1,2,3]
+legendStrs = ['PC'+str(e+1) for e in pcs]
+c = ['r','g','b','y']
+bw = .2
+r = np.arange(1,M+1)
+for i in pcs:    
+    plt.bar(r+i*bw, V[:,i], width=bw)
+plt.xticks(r+bw, attributeNames_PCA)
+plt.xlabel('Attributes')
+plt.ylabel('Component coefficients')
+plt.legend(legendStrs)
+plt.grid()
+plt.title('NanoNose: PCA Component Coefficients')
+plt.show()
+
+# Inspecting the plot, we see that the 2nd principal component has large
+# (in magnitude) coefficients for attributes A, E and H. We can confirm
+# this by looking at it's numerical values directly, too:
+print('PC1:')
+print(V[:,0].T)
+
+# How does this translate to the actual data and its projections?
+# Looking at the data for water:
+
+# Projection of water class onto the 2nd principal component.
+#all_water_data = Y[y==4,:]
+
+#print('First water observation')
+#print(all_water_data[0,:])
+
+# Based on the coefficients and the attribute values for the observation
+# displayed, would you expect the projection onto PC2 to be positive or
+# negative - why? Consider *both* the magnitude and sign of *both* the
+# coefficient and the attribute!
+
+# You can determine the projection by (remove comments):
+#print('...and its projection onto PC2')
+#print(all_water_data[0,:]@V[:,1])
+# Try to explain why?
