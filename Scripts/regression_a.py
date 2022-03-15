f9e62b77dc4245b71c958d74aafbcc0c64499d5f
Sophia Lyngsø <Sophia@liselottesmbp2.students.clients.local>
2022-03-15 17:15:52 +0100

    linear reg i regA

--- Scripts/regression_a.py
+++ Scripts/regression_a.py
@@ -3,13 +3,15 @@
 """
 Created on Tue Mar  8 16:20:39 2022
 
-@author: Sophia
+@author: Sophia Wesche & Simone Engelbrecht
 """
 
 from load_data import *
 
 from scipy.linalg import svd
+import sklearn.linear_model as lm
 import matplotlib.pyplot as plt
+
 
 
 
@@ -18,7 +20,7 @@ glass_type = np.array(X[:, -1], dtype=int).T
 K = glass_type.max() + 1
 glass_type_encoding = np.zeros((glass_type.size, K))
 glass_type_encoding[np.arange(glass_type.size), glass_type] = 1
-X = np.concatenate( (X[:, :-1], glass_type_encoding), axis=1) 
+#X = np.concatenate( (X[:, :-1], glass_type_encoding), axis=1) 
 
 
 
@@ -33,5 +35,40 @@ std = np.std(Y,0)
 print('Mean:', mean, '- std:', std)
 
 
+## Crossvalidation
+# Create crossvalidation partition for evaluation
+#K = 10
+#CV = model_selection.KFold(n_splits=K,shuffle=True)
 
+
+
+cols = range(1, 9) 
+X_reg = X[:, cols]
+y_reg = X[:,0] # -1 takes the last column
+
+
+# Fit ordinary least squares regression model
+model = lm.LinearRegression(fit_intercept=True)
+model = model.fit(X[:, 6],y_reg)
+# Compute model output:
+y_est = model.predict(X[:, 6])
+# Or equivalently:
+#y_est = model.intercept_ + X @ model.coef_
+#w0_est = model.intercept_
+#w1_est = model.model.coef_
+
+# Plot original data and the model output
+attr = 5
+
+
+f = plt.figure()
+
+
+plt.plot(X[:, 6],y_reg,'.')
+#plot(X,y_true,'-')
+plt.plot(X[:, 6],y_est,'-')
+plt.xlabel(attributeNames[6]); plt.ylabel('y')
+plt.legend(['Training data', 'Regression fit (model)'])
+
+plt.show()
 
